6061c1c7dbc1e6b2bdae39e29a1e936aa08eda51
YukTakgi <xgang789@gmail.com>
2021-09-07 14:47:52 +0900

    csv 형식 수정 7번째 줄부터 (header 넣기)

--- RhythmiCar_Unity/Assets/1_Scripts/CSVReader.cs
+++ RhythmiCar_Unity/Assets/1_Scripts/CSVReader.cs
@@ -19,8 +19,8 @@ public class CSVReader
 
         if (lines.Length <= 1) return list;
 
-        var header = Regex.Split(lines[0], SPLIT_RE);
-        for (var i = 1; i < lines.Length; i++)
+        var header = Regex.Split(lines[6], SPLIT_RE);
+        for (var i = 7; i < lines.Length; i++)
         {
 
             var values = Regex.Split(lines[i], SPLIT_RE);
